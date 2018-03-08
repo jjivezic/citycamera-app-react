9fcc68063a38d2a5b224db76bb9b72dc7cc8c8a1
Jelena <ivezic.jj@gmail.com>
2018-03-08 11:04:48 +0100

    HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 
 render((
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   ), document.getElementById('root'));
   
 
