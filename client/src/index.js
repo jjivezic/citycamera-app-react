dde04a17b0d489d8d9388b8931c265a288feb17f
Jelena <ivezic.jj@gmail.com>
2018-03-28 14:56:41 +0200

    progres bar axios

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,12 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import './index.css';
+import { loadProgressBar } from 'axios-progress-bar';
+import 'axios-progress-bar/dist/nprogress.css'
 import App from './App';
-
+loadProgressBar();
 render((
+  
     <Router>
       <App />
     </Router>
