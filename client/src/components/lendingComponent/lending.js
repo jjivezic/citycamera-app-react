03bf0718407c2e23cc8ed0cf606a7c14688df3e5
Jelena <ivezic.jj@gmail.com>
2018-04-03 11:16:41 +0200

    image

--- client/src/components/lendingComponent/lending.js
+++ client/src/components/lendingComponent/lending.js
@@ -5,7 +5,9 @@ const Lending = () => {
         <div className="lending-page-container">
             <a className="hiden" href="https://unsplash.com/@zacmeaney?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge"></a>
             <div className="container">
+         
                 <h1>City Camera</h1>
+                <img className="image-cam" src="https://lh3.googleusercontent.com/Gq7rm2gKTjWsEWW00Lz2a9o0QAkmgtopunK39IlRgCT3L4eZWYswraQZ356oWWFR5Q=s180-rw" alt=""/>
                 <p className="lending-subtitle">Lorem ipsum is a pseudo-Latin text used in web design,
                      typography, layout, and printing in place of English to
                      emphasise design elements over content. It's also called
